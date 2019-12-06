751a6dc5c1c061cc50cc9dba6ba037608a34d275
Andhersson Salazar <andhersson.salazar@gmail.com>
2019-12-05 19:03:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CompiladorLenguajeJACS
+ LenguajeJACS
---
+++ src/compiladorlenguajejacs/CompiladorLenguajeJACS.java
@@ -0,0 +1,14 @@
+package compiladorlenguajejacs;
+
+import compiladorlenguajejacs.interfaces.jfPanelPrincipal;
+
+/**
+ *
+ * @author Andhersson Salazar
+ */
+public class CompiladorLenguajeJACS {
+    public static void main(String[] args) {
+        jfPanelPrincipal a=new jfPanelPrincipal();
+        a.setVisible(true);
+    }
+}
