2a3fa774474951ce70db0951da153cbe9c6c2c6a
Andhersson Salazar <andhersson.salazar@gmail.com>
2019-12-06 15:35:49 -0500

    Version 1.2.1
    Lexico y Sintactico Listo!!!!

--- src/compiladorlenguajejacs/clases/AnalizadorSintactico.java
+++ src/compiladorlenguajejacs/clases/AnalizadorSintactico.java
@@ -8,7 +8,7 @@ import java.util.Stack;
  * @author CHRISTOPER
  */
 enum Modo{
-    ERROR, CORRECTO
+	ERROR, CORRECTO
 }
 
 public class AnalizadorSintactico {
@@ -25,6 +25,10 @@ private Stack<String> pila;
         pila=new Stack<>();
         pila.push("$");
         pila.push("PROGRAMA");
+        
+        pilaMostrar=new LinkedList<>();
+        entradaMostrar=new LinkedList<>();
+        tokenMostrar=new LinkedList<>();
     }
     
     public void Analizar(LinkedList<String> listaInput){
@@ -38,11 +42,11 @@ private Stack<String> pila;
             try {
             	 if(miTabla.Terminal(elementoPila)){
                      if(elementoPila.equals(token)){
-                    	System.out.printf("%1$-100S|%2$-100s|%3$-100S\n",pila.toString(),listaInput.toString(),"Empareja " + token);
+                    	//System.out.printf("%1$-100S|%2$-100s|%3$-100S\n",pila.toString(),listaInput.toString(),"Empareja " + token);
                         pilaMostrar.add(pila.toString());
                         entradaMostrar.add(listaInput.toString());
                         tokenMostrar.add("Empareja " + token);
-                      	listaInput.pollFirst();
+                        listaInput.pollFirst();
                       	pila.pop();                     
                      }else {
                     	 System.out.printf("%1$-100S|%2$-100s|%3$-100S\n",pila.toString(),listaInput.toString(),"ERROR con " + token);
@@ -50,15 +54,15 @@ private Stack<String> pila;
                      }
                  }else if((produccion=miTabla.ExisteInterseccion(elementoPila, token))!=null){
                      	System.out.printf("%1$-100S|%2$-100s|%3$-100S\n",pila.toString(),listaInput.toString(),produccion.toString());
-                        pila.pop();
-                        if(!produccion.getFirst().equals("&")) {
-                            while(!produccion.isEmpty()) {
-                                pila.push(produccion.pollLast());
-                            }
-                            for(int i=produccion.size()-1;i>=0;i--){
-                                pila.push(produccion.get(i));
-                            }   
-                 	}		        
+                 		pila.pop();
+                 		if(!produccion.getFirst().equals("&")) {
+                 			while(!produccion.isEmpty()) {
+                 				pila.push(produccion.pollLast());
+                 			}
+                     	for(int i=produccion.size()-1;i>=0;i--){
+                             pila.push(produccion.get(i));
+                         }   
+                 		}		        
                  }else{
                 	 finalizarPrograma("No existe intersecci�n de "+elementoPila+" y "+token+" en la tabla de simbolos",Modo.ERROR);
                  }
@@ -72,11 +76,12 @@ private Stack<String> pila;
     }
     void finalizarPrograma(String mensaje,Modo modo) {
     	if(modo==Modo.CORRECTO) {
-            System.out.printf("%1$-100S|%2$-100S|%3$-100S\n","[$]","[$]",mensaje);
+    		System.out.printf("%1$-100S|%2$-100S|%3$-100S\n","[$]","[$]",mensaje);
     	}else {
-            System.out.printf("%1$-100S|%2$-100S|%3$-100S\n",mensaje,mensaje,mensaje);
-            System.exit(0);
+    		System.out.printf("%1$-100S|%2$-100S|%3$-100S\n",mensaje,mensaje,mensaje);
+                System.exit(0);
     	}
+    	
     }
     public LinkedList<String> obtenerPilaMostrar(){
         return pilaMostrar;
