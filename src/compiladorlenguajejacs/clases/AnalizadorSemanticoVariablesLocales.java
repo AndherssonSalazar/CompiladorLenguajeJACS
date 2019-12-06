d66824e1b88bc273fcab1c80c8d1b5cf9b6c9aee
crisatuncarayllon <58598125+crisatuncarayllon@users.noreply.github.com>
2019-12-06 12:40:34 -0500

    --

--- src/compiladorlenguajejacs/clases/AnalizadorSemanticoVariablesLocales.java
+++ src/compiladorlenguajejacs/clases/AnalizadorSemanticoVariablesLocales.java
@@ -17,12 +17,48 @@ public class AnalizadorSemanticoVariablesLocales {
     LinkedList<String> temp = new LinkedList<>();
     Bloque inicio ;
     public AnalizadorSemanticoVariablesLocales(){
+        int op = 0;
+        switch(op){
+            case 0:
                 if(cadena.getFirst().id.equals("{")){
                     cadena.pop();
-                    while(cadena.getFirst().id.equals("}")){
-                        temp.add(cadena.pop().id);
-                    }
+                    op=1;
+                }else{
+                    cadena.pop();
+                    op=0;
+                }
+                break;
+            case 1:
+                if(!cadena.getFirst().id.equals("{")){
+                    temp.add(cadena.pop().id);
+                    op=1;
+                }else{
                     inicio = new Bloque(temp);
+                    temp.removeAll(temp);
+                    op=2;
+                }
+                break;
+            case 2:
+                if(!cadena.getFirst().id.equals("{")){
+                    temp.add(cadena.pop().id);
+                    op=2;
+                }else{
+                    inicio.InsertarSubBloque(temp);
+                    temp.removeAll(temp);
+                    op=3;
+                }
+                break;
+            case 3:
+                if(!cadena.getFirst().id.equals("{")){
+                    temp.add(cadena.pop().id);
+                    op=3;
+                }else{
+                    inicio.retornarSubBloque().InsertarSubBloque(temp);
+                    temp.removeAll(temp);
+                    op=4;
+                }
+                break;
         }
+
     }
 }
--- src/compiladorlenguajejacs/clases/Bloque.java
+++ src/compiladorlenguajejacs/clases/Bloque.java
@@ -5,10 +5,46 @@
  */
 package compiladorlenguajejacs.clases;
 
+import com.sun.org.apache.bcel.internal.generic.AALOAD;
+import java.util.LinkedList;
+
 /**
  *
  * @author CHRISTOPER
  */
 public class Bloque {
+    private LinkedList<String> cadena= new LinkedList<>();
+    private Bloque subBloque;
+    public Bloque (LinkedList<String> a){
+        this.cadena = a;
+    }
+    public void InsertarSubBloque(LinkedList<String> b){
+        subBloque= new Bloque(b);
+    }
+    
+    public Bloque retornarSubBloque(){
+        return subBloque;
+    }
+    
+
+    
+    /*
+            public Nodo cabeza;
+    private class Nodo{
+        public LinkedList<Nodo> subloque = new LinkedList<>();
+        public String bloque ;
+        public Nodo(String informacion){
+            subloque.add(new Nodo(informacion));
+        }
+
+    }
+    
+    public void crearBloque(){
+        
+    }
+    */
+
+
+    
     
 }
